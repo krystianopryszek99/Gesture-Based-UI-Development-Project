0aa075c3bbae30959770fdf98a685bda026b857a
Gareth O'Leary <G00375321@gmit.ie>
2022-04-30 23:19:37 +0100

    Moving the player with Socket Communication

--- Python/Detection.py
+++ Python/Detection.py
@@ -7,9 +7,16 @@ PORT_NUMBER = 8080
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSock.connect((SERVER_IP,PORT_NUMBER))
 
+def list2str(inList):
+    outStr = ",".join(str(e) for e in inList)
+    return outStr
+
+i = -5
 while True:
-    message = "Hello from server"
+    message = [i + 0.01]
+    message = list2str(message)
+    
     clientSock.send(message.encode('utf-8'))
     print("Sending: " + message)
-   
-    time.sleep(5.0)
+    i += 1
+    time.sleep(1)
--- Unity/Assets/_Scripts/NetworkManager.cs
+++ Unity/Assets/_Scripts/NetworkManager.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using UnityEngine;
+using System;
 
 public class NetworkManager : MonoBehaviour
 {
     [SerializeField]
     private string clientIp = "127.0.0.1";
-
     [SerializeField]
     private int clientPort = 8080;
 
     int bufferSize = 256;
-
     UdpClient udpClient;
-
     IPEndPoint ipEP;
-
     string message;
-
     byte[] data;
+
+    [SerializeField]
+    public Vector3 object_translation = new Vector3();
 
     // Start is called before the first frame update
     void Start()
@@ -35,8 +33,29 @@ public class NetworkManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        data = udpClient.Receive(ref ipEP);
-        message = Encoding.ASCII.GetString(data, 0, data.Length);
-        Debug.Log("Received " + message);
+        if (udpClient.Available > 0)
+        {
+            data = udpClient.Receive(ref ipEP);
+            message = Encoding.ASCII.GetString(data, 0, data.Length);
+            Debug.Log("Received " + message);
+            UpdateTranslation(message);
+            MoveObject(object_translation);
+        }
+        
+    }
+
+    private void UpdateTranslation(string input)
+    {
+        string[] buffer;
+        buffer = input.Split(',');
+        object_translation.x = (float)Convert.ToDouble(buffer[0]);
+
+        
+    }
+
+    private void MoveObject(Vector3 translation)
+    {
+        transform.position = translation;
+        
     }
 }
