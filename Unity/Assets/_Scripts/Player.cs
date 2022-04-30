9b3f092585b0e02317bb0ff064b43237f1ccc7e2
Krystian Opryszek <kopryszek@gmail.com>
2022-04-30 15:17:19 +0100

    Added layout of the court and player movement

---
+++ Unity/Assets/_Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Translate(0.5f, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(-0.5f, 0, 0);
+        }
+    }
+}
