877bf7ba78ea180c2434aa58a0201e73f165f8f6
Krystian Opryszek <kopryszek@gmail.com>
2022-05-01 14:37:25 +0100

    Created AI opponent movement

---
+++ Unity/Assets/_Scripts/Opponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opponent : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    public float speed;
+    float waypointRadius = 1;
+    
+    void Update()
+    {
+        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < waypointRadius)
+        {
+            current++;
+            if(current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        // Moves towards the waypoints.
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
--- Unity/Assets/_Scripts/Player.cs
+++ Unity/Assets/_Scripts/Player.cs
@@ -6,12 +6,6 @@ public class Player : MonoBehaviour
     public Transform targetPosition;
     float force = 15;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
