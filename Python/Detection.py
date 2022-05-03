1e5a986cf820cc159c058581884035ffd5ded0e7
Gareth O'Leary <G00375321@gmit.ie>
2022-05-03 18:05:12 +0100

    Improved performance of colour detection

--- Python/Detection.py
+++ Python/Detection.py
@@ -13,56 +13,44 @@ print("Server IP:", SERVER_IP)
 print("Port Number:", PORT_NUMBER)
 cap = cv2.VideoCapture(0)
 
-# Colour
-lower = np.array([100,100,60])
-upper = np.array([150,255,200])
-
-
 while True:
     # Captures the live stream frame-by-frame
     ret, frame = cap.read()
+    blur_frame = cv2.GaussianBlur(frame, (5, 5), 0)
     # Converts images from BGR to HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+    # Colour
+    lower = np.array([102, 80, 2])
+    upper = np.array([126, 255, 255])
     # Masking the image to find our color
     mask = cv2.inRange(hsv_frame, lower, upper)
+    # The bitwise and of the frame and mask is done so 
+    # that only the blue coloured objects are highlighted 
+    blue = cv2.bitwise_and(frame, frame, mask=mask)
+    # Finding contours in mask image
+    contours,hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # Finding position of all contours
+    for contour in contours:
+        area = cv2.contourArea(contour)
 
-     # Finding contours in mask image
-    contours,hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if area > 5000:
+            cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
+            # extract moments of the circle
+            M = cv2.moments(contour)
+            # extract the moment values
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            print(cx,cy)
+            message=str((cx-320)*(5.7/320))
+            #Converting string to Byte, and sending it to C#
+            clientSock.send(message.encode('utf-8'))
+            # draw the book contour
+            cv2.circle(frame, (cx, cy), 7, (255, 255, 255), -1)
+            cv2.putText(frame, "Blue", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 255, 255), 3)
 
-     # Finding position of all contours
-    if len(contours) != 0:
-      try:
-        # get the largest contour and its center 
-        c = max(contours, key=cv2.contourArea)
-         # extract moments of the circle
-        M = cv2.moments(c)
-       
-        # extract the moment values
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-
-        print(cx,cy)
-        x,y,w,h = cv2.boundingRect(c)
-        # draw the book contour
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3) 
-
-        message=str((cx-320)*(5.7/320))
-        #Converting string to Byte, and sending it to C#
-        clientSock.send(message.encode('utf-8'))
-        
-      except:
-           clientSock.send(message.encode('utf-8'))
-   
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Blue", blue)
 
     key = cv2.waitKey(1)
     if key == 27:
         break
-
-# release the captured frame
-cap.release()
-# Destroys all of the HighGUI windows.
-cv2.destroyAllWindows()
