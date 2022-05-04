dd5165b49b5d1d5110284559d11a9f2c311ebea1
Gareth O'Leary <G00375321@gmit.ie>
2022-05-04 13:01:20 +0100

    Detection of hand complete

--- Python/Detection.py
+++ Python/Detection.py
@@ -2,6 +2,7 @@ import socket
 import time
 import numpy as np
 import cv2
+import mediapipe as mp
 
 SERVER_IP = '127.0.0.1'
 PORT_NUMBER = 8080
@@ -13,10 +14,43 @@ print("Server IP:", SERVER_IP)
 print("Port Number:", PORT_NUMBER)
 cap = cv2.VideoCapture(0)
 
+# initialize mediapipe
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+pTime = 0
+cTime = 0
 while True:
     # Captures the live stream frame-by-frame
     ret, frame = cap.read()
-    blur_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    start = time.time()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Process the image and find hands
+    results = hands.process(frame)
+    print(results.multi_hand_landmarks)
+    # checking whether a hand is detected
+    if results.multi_hand_landmarks:
+        # working with each hand
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                # find the height, width, and channel of our image
+                h, w, c = frame.shape
+                # get the central positions of the identified hand points.
+                centerx, centery = int(lm.x *w), int(lm.y*h)
+                # locate the key points and highlight the dots in the keypoints
+                cv2.circle(frame, (centerx,centery), 3, (255,0,255), cv2.FILLED)
+            # connect the key points
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(frame,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    cv2.imshow("Frame", frame)
+    
+    """blur_frame = cv2.GaussianBlur(frame, (5, 5), 0)
     # Converts images from BGR to HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # Colour
@@ -49,7 +83,7 @@ while True:
             cv2.putText(frame, "Blue", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 255, 255), 3)
 
     cv2.imshow("Frame", frame)
-    cv2.imshow("Blue", blue)
+    cv2.imshow("Blue", blue)"""
 
     key = cv2.waitKey(1)
     if key == 27:
